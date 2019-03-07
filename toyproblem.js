2e38e5e9545591dcb91aabc9bfd97094282d230f
unknown <honorpurpose@gmail.com>
2019-03-06 18:26:07 -0700

    initial commit

---
+++ toyproblem.js
@@ -0,0 +1,9 @@
+// Todays toy problem is to create a javascript object that would represent something in the room you are in
+const bookshelf = {
+    name: "bookshelf",
+    color: "white",
+    height: 10,
+    condition: "good"
+};
+
+console.log ("A", bookshelf.color, bookshelf.name, "that is", bookshelf.height, "feet tall and in", bookshelf.condition, "condition")
